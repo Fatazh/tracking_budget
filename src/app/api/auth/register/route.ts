b317adb75075e24768d592eb874d66c9ee56d83a
LAPTOP-C89085J0\2080 <majidilkhithar@hotmail.com>
2025-09-16 17:45:19 +0700

    pert

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,8 +1,7 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+﻿import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   const { email, password, name } = await req.json();
